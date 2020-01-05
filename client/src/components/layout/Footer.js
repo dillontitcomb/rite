9d6c4078ce2ad912eee6d34fdbbcc0df7be757ae
Dillon Titcomb <dillontitcomb@gmail.com>
2020-01-05 11:48:35 -0800

    change a few instances of class to className.

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <div class="footer">
-      <div>
-        <p class=" text-center">© 2019 RITE Editions</p>
-        <div className="social-media text-center">
-          <a class="social-media-link" href="www.facebook.com/riteeditions">
-            <i className="fa fa-facebook"></i>
-          </a>
-          <a class="social-media-link" href="www.instagram.com/rite_editions">
-            <i className="fa fa-instagram"></i>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='footer'>
+			<div>
+				<p className=' text-center'>© 2019 RITE Editions</p>
+				<div className='social-media text-center'>
+					<a className='social-media-link' href='www.facebook.com/riteeditions'>
+						<i className='fa fa-facebook'></i>
+					</a>
+					<a
+						className='social-media-link'
+						href='www.instagram.com/rite_editions'>
+						<i className='fa fa-instagram'></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
