b5f4bccf7452cb242befc89de7ae0744ebbbc3a6
Dillon Titcomb <dillontitcomb@gmail.com>
2020-01-05 12:08:19 -0800

    connect mongoDB.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect to DB
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello from your express API'));
 
