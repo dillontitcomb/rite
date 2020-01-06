427bb12539e5af1a374fc6d2ca4292973a0157fa
Dillon Titcomb <dillontitcomb@gmail.com>
2020-01-05 20:38:49 -0800

    add bcrypt and succesfully post user to db with encrypted password.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route GET api/users
 // @desc Test route
@@ -22,14 +25,35 @@ router.post(
 			'Please add a password with more than 6 characters'
 		).isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		console.log(req.body);
+		// Check for errors
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
+		// Get req parameters
+		const { name, email, password } = req.body;
 
-		res.send('Users route!');
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exists' }] });
+			}
+
+			user = new User({ name, email, password });
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
+			res.send('User Successfully Registered!');
+		} catch (err) {
+			console.error(err.message);
+			return res.status(500).send('Server Error');
+		}
 	}
 );
 
