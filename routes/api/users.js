41c959dd0105a54cecaecf1b10593b23ddc2a457
Dillon Titcomb <dillontitcomb@gmail.com>
2020-01-05 20:56:58 -0800

    add jwt and generate token with each user registration.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -26,7 +28,6 @@ router.post(
 		).isLength({ min: 6 })
 	],
 	async (req, res) => {
-		console.log(req.body);
 		// Check for errors
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
@@ -47,9 +48,22 @@ router.post(
 			user = new User({ name, email, password });
 
 			const salt = await bcrypt.genSalt(10);
+
 			user.password = await bcrypt.hash(password, salt);
+
 			await user.save();
-			res.send('User Successfully Registered!');
+
+			const payload = { user: { id: user.id } };
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 36000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			return res.status(500).send('Server Error');
