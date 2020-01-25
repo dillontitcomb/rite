a06e6532c39f94db7c89154ade412fdb1221d285
Dillon Titcomb <dillontitcomb@gmail.com>
2020-01-24 16:06:18 -0800

    transition nav and footer to white style.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,9 +2,22 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
 
-//Don't use <a> tags for client-side routing (clears app state!)
+const updateNavLinks = () => {
+  const links = document.getElementsByClassName('nav-item');
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function() {
+      let activeLink = document.getElementsByClassName('nav-active');
+      activeLink[0].className = activeLink[0].className.replace(
+        ' nav-active',
+        ''
+      );
+      this.className += ' nav-active';
+    });
+  }
+};
 
 const Navbar = () => {
+  updateNavLinks();
   const authContext = useContext(AuthContext);
   const { isAuthenticated, logout } = authContext;
 
@@ -14,16 +27,16 @@ const Navbar = () => {
   };
   return (
     <div>
-      <nav className="navbar bg-primary">
+      <nav className="navbar bg-white">
         <Link to="/">
           <img
             className="rite-logo"
-            src="/img/fb-logo-sm.jpg"
+            src="/img/logo-on-white.png"
             alt="RITE Editions Logo"
           />
         </Link>
         <ul>
-          <Link className="nav-item" to="/">
+          <Link className="nav-item nav-active" to="/">
             Home
           </Link>
           <Link className="nav-item" to="/about">
