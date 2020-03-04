0492f94dee276aaabfd7d7aacf36f5a5a20f54f7
Dillon Titcomb <dillontitcomb@gmail.com>
2020-03-04 12:03:08 -0800

    refactor Artists page to show artist cards including name, bio, and artistLink.

--- client/src/components/artists/Artists.js
+++ client/src/components/artists/Artists.js
@@ -1,35 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
-import EditionContext from '../../context/edition/editionContext';
-import EditionItem from '../editions/EditionItem';
+import React, { useContext, useEffect } from 'react';
+import ArtistContext from '../../context/artist/artistContext';
 import './Artists.css';
 
 const Artists = () => {
-  const [state, setState] = useState({
-    displayEditions: false,
-    artistEditions: []
-  });
-
-  const editionContext = useContext(EditionContext);
-  const { getEditions, editions } = editionContext;
+  const artistContext = useContext(ArtistContext);
+  const { getArtists, artists } = artistContext;
 
   useEffect(() => {
-    getEditions();
+    getArtists();
     //eslint-disable-next-line
   }, []);
-
-  const artists = editions.reduce((acc, val) => {
-    acc.push(...val.artists);
-    return acc;
-  }, []);
-
-  // Show artist's editions when artist name is selected
-  const handleSelectArtist = (e) => {
-    e.preventDefault();
-    let artistEditions = editions.filter((edition) =>
-      edition.artists.includes(e.target.value)
-    );
-    setState({ displayEditions: true, artistEditions: artistEditions });
-  };
 
   return (
     <div className="container">
@@ -37,20 +17,23 @@ const Artists = () => {
       <div className="artists-container text-center">
         {artists &&
           artists.map((artist, key) => (
-            <button
-              value={artist}
-              className="btn btn-sm artists-name"
-              key={key}
-              onClick={handleSelectArtist}
-            >
-              {artist}
-            </button>
-          ))}
-      </div>
-      <div className="artists-edition-display my-2">
-        {state.displayEditions &&
-          state.artistEditions.map((edition, key) => (
-            <EditionItem edition={edition} key={key}></EditionItem>
+            // TODO: Create ArtistItem component and use here
+            <div className="card">
+              <p className="lead" key={key}>
+                {artist.name}
+              </p>
+              <p>{artist.bio}</p>
+              <p>
+                Check out their work at{' '}
+                <a
+                  href={artist.artistLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {artist.artistLink}
+                </a>
+              </p>
+            </div>
           ))}
       </div>
     </div>
