2c38329ecd7de7e338d0bbfd03d2e6f4eeff5d76
Dillon Titcomb <dillontitcomb@gmail.com>
2020-08-26 17:17:56 -0700

    change background image on homepage.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
-	return (
-		<Link to='/editions'>
-			<div className='home-header-backdrop'></div>
-		</Link>
-	);
+  return (
+    <Link to="/editions">
+      <div className="home-header-backdrop"></div>
+    </Link>
+  );
 };
 
 export default Home;
