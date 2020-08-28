4cb101c56bdef0439e0fd64bf643302ea9d4beda
Dillon Titcomb <dillontitcomb@gmail.com>
2020-08-28 11:34:27 -0700

    add heroku postbuild script, and server code to serve static assets.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const fileUpload = require('express-fileupload');
 const app = express();
+const path = require('path');
 
 // Connect to DB
 
@@ -12,8 +13,6 @@ app.use(fileUpload());
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello from your express API'));
-
 // Define Routes
 app.use('/api/editions', require('./routes/api/editions'));
 app.use('/api/users', require('./routes/api/users'));
@@ -22,9 +21,13 @@ app.use('/api/upload', require('./routes/api/upload'));
 app.use('/api/newsPosts', require('./routes/api/newsPosts'));
 app.use('/api/artists', require('./routes/api/artists'));
 
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder for build
   app.use(express.static('client/build'));
+  // Serve built index.html file
   app.get('*', (req, res) => {
+    // Go from current directory (dirname) to client folder, to build folder, then load index.html
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
