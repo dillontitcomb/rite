41b56f5fd39364ff570caf7221a8d77968f128a3
Dillon Titcomb <dillontitcomb@gmail.com>
2021-03-11 11:52:32 -0600

    add improved homepage image.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,42 +4,62 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="container home-container">
-      <div className="home-showcase my-2">
-        <Link to="/editions">
+    <div className='container home-container'>
+      <div className='home-showcase my-2'>
+        <Link to='/editions'>
           <img
-            className="home-image"
-            src="/img/othello_1.jpg"
-            alt="Othello Jigsaw Puzzle"
+            className='home-image'
+            src='/img/improved_rite_image.jpeg'
+            alt='Othello Jigsaw Puzzle'
           />
         </Link>
-        <p className="large my-2">
+        <p className='large my-2'>
           Woody De Othello,
           <strong>
             {' '}
-            <i>Jigsaw Puzzle, 2020-21</i> 
+            <i>Jigsaw Puzzle, 2020-21</i>
           </strong>
         </p>
         <p>
-          The limited-edition jigsaw puzzles are hand cut by Mark A. Rodriquez and Soulfrawg Puzzles. Each puzzle varies in the number of pieces and the configuration. The puzzles are intended to be challenging. There are no images or straight edges to guide the user. Woody De Othello’s design will be revealed when the owner completes the puzzle. 
+          The limited-edition jigsaw puzzles are hand cut by Mark A. Rodriquez
+          and Soulfrawg Puzzles. Each puzzle varies in the number of pieces and
+          the configuration. The puzzles are intended to be challenging. There
+          are no images or straight edges to guide the user. Woody De Othello’s
+          design will be revealed when the owner completes the puzzle.
         </p>
-        <p className="lead-sm my-1">
-          <Link to="/editions/6035c4373a86003d60a914ed">Learn More →</Link>
+        <p className='lead-sm my-1'>
+          <Link to='/editions/6035c4373a86003d60a914ed'>Learn More →</Link>
         </p>
       </div>
-      <div className="home-upcoming">
-        <p className="large my-2">New Edition Addition</p>
-        <p className="lead-sm my-1"><strong>Hung Liu, Jeffry Mitchell, and Leah Rosenberg, <em>Face Mask Project, </em>2020-2021</strong></p>
-        <p className="lead-sm my-1">Set of 3 Masks</p>
-        <p className="my-1">In response to the Covid-19 crisis, the Face Mask Project, 2020 was developed to benefit the Artist Relief Fund (artistrelief.org) and the World Central Kitchen (wck.org). Artists, Hung Liu, Jeffry Mitchell and Leah Rosenberg each created face covering designs. The compositions were digitally transposed onto cotton, then sewn into non-surgical face masks. The face masks are co-produced with RITE Editions and the Rena Bransten Gallery in collaboration with Stevie Howell.</p>
+      <div className='home-upcoming'>
+        <p className='large my-2'>New Edition Addition</p>
+        <p className='lead-sm my-1'>
+          <strong>
+            Hung Liu, Jeffry Mitchell, and Leah Rosenberg,{' '}
+            <em>Face Mask Project, </em>2020-2021
+          </strong>
+        </p>
+        <p className='lead-sm my-1'>Set of 3 Masks</p>
+        <p className='my-1'>
+          In response to the Covid-19 crisis, the Face Mask Project, 2020 was
+          developed to benefit the Artist Relief Fund (artistrelief.org) and the
+          World Central Kitchen (wck.org). Artists, Hung Liu, Jeffry Mitchell
+          and Leah Rosenberg each created face covering designs. The
+          compositions were digitally transposed onto cotton, then sewn into
+          non-surgical face masks. The face masks are co-produced with RITE
+          Editions and the Rena Bransten Gallery in collaboration with Stevie
+          Howell.
+        </p>
         <img
-            className="home-additional-image"
-            src="/img/face_mask_000.jpg"
-            alt="Face Mask Project"
-          />
-        <p className="my-1"><i>*For purchase, navigate to the Buy RITE tab.</i></p>
-        <p className="lead-sm my-1">
-          <Link to="/editions">View Editions →</Link>
+          className='home-additional-image'
+          src='/img/face_mask_000.jpg'
+          alt='Face Mask Project'
+        />
+        <p className='my-1'>
+          <i>*For purchase, navigate to the Buy RITE tab.</i>
+        </p>
+        <p className='lead-sm my-1'>
+          <Link to='/editions'>View Editions →</Link>
         </p>
       </div>
     </div>
